e7bea7df54d3033e7ebd1fcc387618c386388b00
Aditya Suryandaru <suryandarua@gmail.com>
2026-01-20 08:16:55 +0800

    feat: create model Ticket

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    
+    protected $table = 'tickets';
+    protected $fillable = [
+        'id',
+        'title',
+        'description',
+        'category',
+        'priority',
+        'status',
+        'reporter_id',
+        'assigned_to_id',
+        'assigned_team',
+        'date_reported',
+        'due_date',
+        'resolved_date',
+        'closed_date',
+        'sla_breached',
+        'response_time',
+        'resolution_time',
+        'estimated_effort',
+        'actual_effort',
+        'parent_ticket_id',
+        'converted_to_type',
+        'converted_to_id',
+        'converted_at',
+        'converted_by',
+        'conversion_reason',
+        'created_at',
+        'updated_at',
+    ];
+
 }
