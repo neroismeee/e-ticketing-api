84f41c06443d4b36284e7651c26012a51a7b4d6b
Aditya Suryandaru <suryandarua@gmail.com>
2026-01-22 14:17:12 +0800

    feat: add validation in ErrorRequest

--- app/Http/Requests/ErrorRequest.php
+++ app/Http/Requests/ErrorRequest.php
@@ -3,6 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Models\ErrorReport;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;   
 
 class ErrorRequest extends FormRequest
 {
@@ -11,7 +14,7 @@ class ErrorRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +25,36 @@ class ErrorRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'category' => 'required|string|in:' . implode(',', ErrorReport::CATEGORIES),
+            'priority' => 'required|string|in:' . implode(',', ErrorReport::PRIORITIES),
+            'status' => 'required|string|in:' . implode(',', ErrorReport::STATUSES),
+            'reporter_id' => 'required|integer',
+            'assigned_to_id' => 'nullable|integer',
+            'assigned_team' => 'nullable|string|max:255|in:' . implode(',', ErrorReport::TEAMS),
+            'date_reported' => 'required|date',
+            'start_date' => 'nullable|date',
+            'due_date' => 'nullable|date',
+            'completion_date' => 'nullable|date',
+            'estimated_effort' => 'nullable|integer',
+            'actual_effort' => 'nullable|integer',
+            'sla_time_elapsed' => 'nullable|integer',
+            'sla_time_remaining' => 'nullable|integer',
+            'sla_breached' => 'required|boolean',
+            'source_ticket_id' => 'nullable|integer',
+            'is_direct_input' => 'required|boolean',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'status' => false,
+                'message' => 'Validation error',
+                'errors' => $validator->errors()
+            ], 422)
+        );
     }
 }
