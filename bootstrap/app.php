c6dbd54f34e3beb1a04973f7fda99e0e6885ee3a
Aditya Suryandaru <suryandarua@gmail.com>
2026-01-28 10:53:22 +0800

    refactor: install laravel breeze

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+<<<<<<< HEAD
         // $middleware->api(prepend: [
         //     \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         // ]);
@@ -20,6 +21,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
 
+=======
+>>>>>>> parent of 8459182 (feat: install laravel breeze)
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
