4502399cec0577db007e47d009e5b4077d9e17c6
Aditya Suryandaru <suryandarua@gmail.com>
2026-01-29 20:58:17 +0800

    feat: create register API

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Api\v1\ErrorController;
 use App\Http\Controllers\Api\v1\TicketController;
 use App\Http\Controllers\Api\v1\FeatureController;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
@@ -17,19 +17,20 @@ Route::prefix('v1')->group(function () {
     Route::post('/tickets', [TicketController::class, 'store']);
     Route::put('/tickets/{id}', [TicketController::class, 'update']);
     Route::delete('/tickets/{id}', [TicketController::class, 'destroy']);
-    
+
     //error report routes
     Route::get('/error-reports', [ErrorController::class, 'index']);
     Route::get('/error-reports/{id}', [ErrorController::class, 'show']);
     Route::post('/error-reports', [ErrorController::class, 'store']);
     Route::put('/error-reports/{id}', [ErrorController::class, 'update']);
     Route::delete('/error-reports/{id}', [ErrorController::class, 'destroy']);
-    
+
     //feature request routes
     Route::get('/feature-requests', [FeatureController::class, 'index']);
     Route::get('/feature-requests/{id}', [FeatureController::class, 'show']);
     Route::post('/feature-requests', [FeatureController::class, 'store']);
     Route::put('/feature-requests/{id}', [FeatureController::class, 'update']);
     Route::delete('/feature-requests/{id}', [FeatureController::class, 'destroy']);
-    
 });
+
+require __DIR__ . '/auth.php';
