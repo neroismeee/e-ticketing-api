8406330a74066b344add810c4d10aee86cb8249d
Aditya Suryandaru <suryandarua@gmail.com>
2026-02-02 14:56:04 +0800

    feat: create role middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -19,9 +20,10 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+            'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
         
-        //
+        
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/api.php
+++ routes/api.php
@@ -6,31 +6,44 @@ use App\Http\Controllers\Api\v1\ErrorController;
 use App\Http\Controllers\Api\v1\TicketController;
 use App\Http\Controllers\Api\v1\FeatureController;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::prefix('v1')->group(function () {
 
-Route::prefix('v1')->group(function () {
-    //ticket routes
-    Route::get('/tickets', [TicketController::class, 'index']);
-    Route::get('/tickets/{id}', [TicketController::class, 'show']);
-    Route::post('/tickets', [TicketController::class, 'store']);
-    Route::put('/tickets/{id}', [TicketController::class, 'update']);
-    Route::delete('/tickets/{id}', [TicketController::class, 'destroy']);
+        Route::middleware('role:admin, it_staff')->group(function () {
+            //ticket routes
+            Route::get('/tickets', [TicketController::class, 'index']);
+            Route::get('/tickets/{id}', [TicketController::class, 'show']);
+            
+            //error report routes
+            Route::get('/error-reports', [ErrorController::class, 'index']);
+            Route::get('/error-reports/{id}', [ErrorController::class, 'show']);
+            
+            //feature request routes
+            Route::get('/feature-requests', [FeatureController::class, 'index']);
+            Route::get('/feature-requests/{id}', [FeatureController::class, 'show']);
+        });
 
-    //error report routes
-    Route::get('/error-reports', [ErrorController::class, 'index']);
-    Route::get('/error-reports/{id}', [ErrorController::class, 'show']);
-    Route::post('/error-reports', [ErrorController::class, 'store']);
-    Route::put('/error-reports/{id}', [ErrorController::class, 'update']);
-    Route::delete('/error-reports/{id}', [ErrorController::class, 'destroy']);
+        Route::middleware('role:it_staff')->group(function () {
+            //ticket routes
+            Route::post('/tickets', [TicketController::class, 'store']);
+            Route::put('/tickets/{id}', [TicketController::class, 'update']);
+            Route::delete('/tickets/{id}', [TicketController::class, 'destroy']);
 
-    //feature request routes
-    Route::get('/feature-requests', [FeatureController::class, 'index']);
-    Route::get('/feature-requests/{id}', [FeatureController::class, 'show']);
-    Route::post('/feature-requests', [FeatureController::class, 'store']);
-    Route::put('/feature-requests/{id}', [FeatureController::class, 'update']);
-    Route::delete('/feature-requests/{id}', [FeatureController::class, 'destroy']);
+            //error report routes
+            Route::post('/error-reports', [ErrorController::class, 'store']);
+            Route::put('/error-reports/{id}', [ErrorController::class, 'update']);
+            Route::delete('/error-reports/{id}', [ErrorController::class, 'destroy']);
+
+            //feature request routes
+            Route::post('/feature-requests', [FeatureController::class, 'store']);
+            Route::put('/feature-requests/{id}', [FeatureController::class, 'update']);
+            Route::delete('/feature-requests/{id}', [FeatureController::class, 'destroy']);
+        });
+    });
 });
+
 
 require __DIR__ . '/auth.php';
