c7f16cd78f771445e098e086a74751b4c1f99945
Aditya Suryandaru <suryandarua@gmail.com>
2026-02-11 20:35:08 +0800

    feat: create ApiResponse

---
+++ app/Helpers/ApiResponse.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+
+class ApiResponse
+{
+    public static function success(
+        $data = null,
+        string $message = 'Success',
+        int $code = 200,
+        $meta = null
+    ): JsonResponse {
+        $response = [
+            'status' => true,
+            'message' => $message,
+            'data' => $data
+        ];
+        
+        if ($meta) {
+            $response['meta'] = $meta;
+        }
+
+        return response()->json($response, $code);  
+    }
+
+    public static function paginated(
+        LengthAwarePaginator $paginator,
+        $resource,
+        string $message = 'Data Retrieved Successfully'
+    ): JsonResponse {
+        return response()->json([
+            'status' => true,
+            'message' => $message,
+            'data' => $resource,
+            'meta' => [
+                'current_page' => $paginator->currentPage(),
+                'last_page' => $paginator->lastPage(),
+                'per_page' => $paginator->perPage(),
+                'total' => $paginator->total()
+            ],
+        ], 200);
+    }
+
+    public static function error(
+        string $message = 'Error',
+        $errors = null,
+        int $code = 400
+    ): JsonResponse {
+        return response()->json([
+            'status' => false,
+            'message' => $message,
+            'errors' => $errors
+        ], $code);
+    }
+}
+
+
