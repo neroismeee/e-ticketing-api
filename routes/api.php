e3548643fa9a85af4d91ab19e19b7b9948e0c9ff
Aditya Suryandaru <suryandarua@gmail.com>
2026-02-13 20:58:23 +0800

    feat: change parameter for put and delete method

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\v1\ErrorController;
 use App\Http\Controllers\Api\v1\TicketController;
 use App\Http\Controllers\Api\v1\FeatureController;
-use App\Http\Controllers\Api\v1\StatusHistoryController;
 
 Route::prefix('v1')->group(function () {
     require __DIR__ . '/auth.php';
@@ -18,35 +17,35 @@ Route::prefix('v1')->group(function () {
         Route::middleware('role:admin,it_staff,reporter,team_lead')->group(function () {
             //ticket routes
             Route::get('/tickets', [TicketController::class, 'index']);
-            Route::get('/tickets/{id}', [TicketController::class, 'show']);
+            Route::get('/tickets/{ticket}', [TicketController::class, 'show']);
 
             //error report routes
             Route::get('/error-reports', [ErrorController::class, 'index']);
-            Route::get('/error-reports/{id}', [ErrorController::class, 'show']);
+            Route::get('/error-reports/{error}', [ErrorController::class, 'show']);
 
             //feature request routes
             Route::get('/feature-requests', [FeatureController::class, 'index']);
-            Route::get('/feature-requests/{id}', [FeatureController::class, 'show']);
+            Route::get('/feature-requests/{feature}', [FeatureController::class, 'show']);
 
             //status history
-            Route::get('/status-history', [StatusHistoryController::class, 'index']);
+            Route::get('/status-history', [TicketController::class, 'status']);
         });
 
         Route::middleware('role:it_staff')->group(function () {
             //ticket routes
             Route::post('/tickets', [TicketController::class, 'store']);
-            Route::put('/tickets/{id}', [TicketController::class, 'update']);
-            Route::delete('/tickets/{id}', [TicketController::class, 'destroy']);
+            Route::put('/tickets/{ticket}', [TicketController::class, 'update']);
+            Route::delete('/tickets/{ticket}', [TicketController::class, 'destroy']);
 
             //error report routes
             Route::post('/error-reports', [ErrorController::class, 'store']);
-            Route::put('/error-reports/{id}', [ErrorController::class, 'update']);
-            Route::delete('/error-reports/{id}', [ErrorController::class, 'destroy']);
+            Route::put('/error-reports/{error}', [ErrorController::class, 'update']);
+            Route::delete('/error-reports/{error}', [ErrorController::class, 'destroy']);
 
             //feature request routes
             Route::post('/feature-requests', [FeatureController::class, 'store']);
-            Route::put('/feature-requests/{id}', [FeatureController::class, 'update']);
-            Route::delete('/feature-requests/{id}', [FeatureController::class, 'destroy']);
+            Route::put('/feature-requests/{feature}', [FeatureController::class, 'update']);
+            Route::delete('/feature-requests/{feature}', [FeatureController::class, 'destroy']);
         });
     });
 });
