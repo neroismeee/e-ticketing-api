dd4f47b346d1a2e0f182084d011d7086b1e9c445
Aditya Suryandaru <suryandarua@gmail.com>
2026-03-10 11:01:11 +0800

    feat: create comment request

---
+++ app/Http/Requests/Comment/UpdateCommentRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Comment;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCommentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'ticket_id' => 'nullable',
+            'feature_request_id' => 'nullable',
+            'error_report_id' => 'nullable',
+            'user_id' => 'sometimes|integer',
+            'content' => 'sometimes|string',
+            'is_internal' => 'nullable|boolean|',
+            'created_at' => 'nullable|timestamp',
+            'updated_at' => 'nullable|timestamp'
+        ];
+    }
+}
