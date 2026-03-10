5facc4d0739bfe4da0fc4eb630adb3027b6aa021
Aditya Suryandaru <suryandarua@gmail.com>
2026-03-10 11:00:23 +0800

    feat: add fillable and relations

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = [
+        'ticket_id',
+        'error_report_id',
+        'feature_request_id',
+        'user_id',
+        'content',
+        'is_internal',
+        'created_at',
+        'updated_at'
+    ];
+
+    // relations
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class, 'ticket_id');
+    }
+
+    public function error_report()
+    {
+        return $this->belongsTo(ErrorReport::class, 'error_report_id');
+    }
+
+    public function feature_request()
+    {
+        return $this->belongsTo(FeatureRequest::class, 'feature_request_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
