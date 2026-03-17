67d055af430b78f1f139dfb28f7b74a7bc0b9867
Aditya Suryandaru <suryandarua@gmail.com>
2026-03-17 14:00:43 +0800

    feat: create comment mentions migration

---
+++ database/migrations/2026_03_10_124144_create_comment_mentions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comment_mentions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('comment_id')
+                  ->constrained()
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+            $table->foreignId('user_id')
+                  ->nullable()
+                  ->constrained()
+                  ->onUpdate('cascade')
+                  ->nullOnDelete();
+
+            //index
+            $table->index('comment_id');
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comment_mentions');
+    }
+};
