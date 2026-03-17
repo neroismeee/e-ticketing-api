ebe36c317b45ffad3c4f076440b2ee9b40738e08
Aditya Suryandaru <suryandarua@gmail.com>
2026-03-17 14:11:00 +0800

    feat: create comment mentions modell

---
+++ app/Models/CommentMentions.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CommentMentions extends Model
+{
+    protected $fillable = [
+        'id',
+        'comment_id',
+        'user_id'
+    ];
+
+    // relations    
+    public function comment()
+    {
+        return $this->belongsTo(Comment::class, 'comment_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
