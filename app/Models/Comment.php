5bbddefad9601d170a1a72efdbe262c37effcb09
Aditya Suryandaru <suryandarua@gmail.com>
2026-03-28 18:53:35 +0800

    feat: update comment fillable

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,34 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+
+#[Fillable([
+    'ticket_id',
+    'error_report_id',
+    'feature_request_id',
+    'user_id',
+    'content',
+    'is_internal',
+    'created_at',
+    'updated_at'
+])]
 
 class Comment extends Model
 {
-    protected $fillable = [
-        'ticket_id',
-        'error_report_id',
-        'feature_request_id',
-        'user_id',
-        'content',
-        'is_internal',
-        'created_at',
-        'updated_at'
-    ];
-
     // relations
-    public function ticket()
+    public function commentable(): MorphTo
     {
-        return $this->belongsTo(Ticket::class, 'ticket_id');
-    }
-
-    public function error_report()
-    {
-        return $this->belongsTo(ErrorReport::class, 'error_report_id');
-    }
-
-    public function feature_request()
-    {
-        return $this->belongsTo(FeatureRequest::class, 'feature_request_id');
+        return $this->morphTo();
     }
 
     public function user()
