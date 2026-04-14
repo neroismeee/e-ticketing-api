2993bdb9e9401cf0b2e5c5369b7839deec8b8ce5
Aditya Suryandaru <suryandarua@gmail.com>
2026-04-14 20:41:28 +0800

    feat: create mention controller

--- app/Http/Controllers/Api/v1/Comment/MentionController.php
+++ app/Http/Controllers/Api/v1/Comment/MentionController.php
@@ -5,24 +5,25 @@ namespace App\Http\Controllers\api\v1\Comment;
 use App\Helpers\ApiResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Comment\MentionResource;
-use App\Models\CommentMention;
+use App\Models\Comment;
+use App\Services\Comment\MentionService;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class MentionController extends Controller
 {
+    public function __construct(
+        private readonly MentionService $mentionService
+    ) {}
+
     /**
      * Display a listing of the resource.
      */
-    public function index(): JsonResponse
+    public function index(Comment $comment): JsonResponse
     {
-        $mentions = CommentMention::with([
-            'comment_id',
-            'user_id'
-        ])->latest()
-            ->paginate(10);
+        $mentions = $this->mentionService->getByComment($comment);
 
-        return ApiResponse::paginated(
-            $mentions,
+        return ApiResponse::success(
             MentionResource::collection($mentions),
             'Comment Mentions Retrieved Successfully'
         );
@@ -30,6 +31,6 @@ class MentionController extends Controller
 
     public function mine()
     {
-        
+
     }
 }
