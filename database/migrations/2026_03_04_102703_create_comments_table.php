f707d615fbe9950fd6303779c255adf74183d0d4
Aditya Suryandaru <suryandarua@gmail.com>
2026-04-14 20:37:56 +0800

    feat: remove nullable user_id from comments table

--- database/migrations/2026_03_04_102703_create_comments_table.php
+++ database/migrations/2026_03_04_102703_create_comments_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('commentable_id');
             $table->string('commentable_type');
             $table->foreignId('user_id')
-                ->nullable()
                 ->constrained()
                 ->onUpdate('cascade')
                 ->nullOnDelete();
