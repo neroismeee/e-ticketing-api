c834ce612240489ac0d70167161686bc1c2bdeab
Aditya Suryandaru <suryandarua@gmail.com>
2026-04-14 20:38:23 +0800

    feat: add helpers function and mentions relation

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Http\Controllers\api\v1\Comment\MentionController;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
@@ -16,6 +17,17 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 
 class Comment extends Model
 {
+    // helpers
+    public function scopePublic($query)
+    {
+        $query->where('is_internal', false);
+    }
+
+    public function scopeInternal($query)
+    {
+        $query->where('is_internal', true);
+    }
+
     // relations
     public function commentable(): MorphTo
     {
@@ -25,5 +37,10 @@ class Comment extends Model
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function mentions()
+    {
+        return $this->hasMany(CommentMention::class, 'comment_id');
     }
 }
