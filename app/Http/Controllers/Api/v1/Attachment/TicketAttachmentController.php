969dd8b131aec66f416932e2919510c66f7f771b
Aditya Suryandaru <suryandarua@gmail.com>
2026-04-21 13:59:02 +0800

    feat: create ticket attachment controller

---
+++ app/Http/Controllers/Api/v1/Attachment/TicketAttachmentController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Attachment;
+
+use App\Http\Controllers\Controller;
+use App\Models\Attachment;
+use App\Models\Ticket;
+use App\Services\Attachment\AttachmentService;
+use App\Traits\HandleAttachments;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class TicketAttachmentController extends Controller
+{
+    use HandleAttachments;
+
+    protected AttachmentService $attachmentService;
+
+    public function __construct(AttachmentService $attachmentService)
+    {
+        $this->attachmentService = $attachmentService;
+    }
+
+    protected function getAttachmentService(): AttachmentService
+    {
+        return $this->attachmentService;
+    }
+
+    public function index(Request $request, Ticket $ticket): JsonResponse
+    {
+        return $this->indexAttachments($request, $ticket);
+    }
+
+    public function store(Request $request, Ticket $ticket): JsonResponse
+    {
+        return $this->storeAttachments($request, $ticket);
+    }
+
+    public function destroy(Ticket $ticket, Attachment $attachment): JsonResponse
+    {
+        return $this->destroyAttachments($ticket, $attachment);
+    }
+}
