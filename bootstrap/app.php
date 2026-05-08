c701019fea1ce5faf567855568ef8158511a9e64
Aditya Suryandaru <suryandarua@gmail.com>
2026-05-08 15:13:28 +0800

    refactor: delete unnecessary code

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,10 +22,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
 
-        // $middleware->api(prepend: [
-        //     \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        // ]);
-
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
             'role' => \App\Http\Middleware\RoleMiddleware::class,
